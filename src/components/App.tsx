98b023ef7a7960209fc1873e7111995da6948ae1
stacy <chiufin@hotmail.com>
2019-09-26 16:37:31 +0100

    origin code

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => (
+  <>
+    <div>Have a look at the README for instructions</div>
+    <div>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </div>
+  </>
+)
+
+export default App
